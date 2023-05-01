867abe66afb4c54ab9a1e69d07299c611fa23a04
Babatunde Ojo <babatundeojo30@gmail.com>
2023-05-01 22:28:58 +0100

    add assertion

--- cypress/e2e/konga/login.cy.js
+++ cypress/e2e/konga/login.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress" />
 
+// import pages
+import LoginPage from "../../pages/login";
+
 describe("Test Konga login ", () => {
+
+    // create an instance of login class
+    const loginPage = new LoginPage();
 
     it("Test valid login credentials", () => {
 
@@ -8,32 +14,28 @@ describe("Test Konga login ", () => {
         cy.visit("https://www.konga.com/");
 
         // click on login/signup button
-        cy.get("._12e27_1r3kc > ._7ad32_SD12Y").click();
+        loginPage.clickLoginSignupBtn();
 
-        // confirm the login modal is visible
-        let expectedLoginText = "Login";
-        cy.get("h5").then((x) => {
-            let actualLoginText = x.text();
-            expect(actualLoginText).to.equal(expectedLoginText);
-        })
+        // validate login modal is visible
+        loginPage.validateLoginModalVisible("Login");
 
         // enter email address
-        cy.get("#username").type("username@a.com");
+        loginPage.setEmail("a@gmail.com");
 
-        // confirm that the username field have username@a.com as value
-        cy.get("#username").should("have.value", "username@a.com");
+        // validate email field have the expected value
+        loginPage.validateEmailField("a@@gmail.com")
 
         // enter password
-        cy.get("#password").type("pass");
+        loginPage.setPassword("pass");
 
-        // confirm that the password field have pass as value
-        cy.get("#password").should("have.value", "pass");
+        // validate password field have the expected value
+        loginPage.validatePasswordField("pass");
 
         // click on login button
-        cy.get('button[type="submit"]').contains("Login").click();
+        loginPage.clickLoginBtn();
 
-        // confirm that user is on the login screen by checking that the first span have My Account as a text
-        cy.get("._7ad32_SD12Y > span:nth-child(1)").should('have.text', "My Account");
+        // validate user is logged in
+        loginPage.validateUserLogin("My Account");
 
     })
 })
